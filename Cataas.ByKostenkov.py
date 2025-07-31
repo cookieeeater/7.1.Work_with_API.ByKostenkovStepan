1d787b4c20d1a2257608b0bbb9fc8be721905b15
cookieeeater <stepa-kostenkov@mail.ru>
2025-07-31 10:45:09 +0700

    Добавили кнопку дял загрузки нового изображения

--- Cataas.ByKostenkov.py
+++ Cataas.ByKostenkov.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-def load_image():
+def load_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -14,6 +14,13 @@ def load_image():
         print(f"Произошла ошибка: {e}")
         return None
 
+def set_image():
+    img = load_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img
+
 window = Tk()
 window.title("Cats!")
 window.geometry("600x480")
@@ -21,11 +28,12 @@ window.geometry("600x480")
 label = Label()
 label.pack()
 
+update_button = Button(text="Обновить", command=set_image)
+update_button.pack()
+
 url = 'https://cataas.com/cat'
 img = load_image(url)
 
-if img:
-    label.config(image=img)
-    label.image = img
+set_image()
 
 window.mainloop()
