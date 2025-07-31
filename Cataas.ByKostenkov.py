7c3a794f52bdee4085e3f92ceab26cb7301d2bff
cookieeeater <stepa-kostenkov@mail.ru>
2025-07-31 11:15:08 +0700

    Теперь каждое изображение открывается в новом окне

--- Cataas.ByKostenkov.py
+++ Cataas.ByKostenkov.py
@@ -15,12 +15,15 @@ def load_image(url):
         print(f"Произошла ошибка: {e}")
         return None
 
-def set_image():
+def open_new_window():
     img = load_image(url)
-
     if img:
-        label.config(image=img)
+        new_window = Toplevel()
+        new_window.title("Картинка с котиком")
+        new_window.geometry("600x480")
+        label = Label(new_window, image=img)
         label.image = img
+        label.pack()
 
 def exit():
     window.destroy()
@@ -28,9 +31,6 @@ def exit():
 window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
-
-label = Label()
-label.pack()
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
@@ -40,13 +40,11 @@ window.config(menu=menu_bar)
 
 file_menu = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Файл", menu=file_menu)
-file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_command(label="Загрузить фото", command=open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label="Выход", command=exit)
 
 url = 'https://cataas.com/cat'
 img = load_image(url)
-
-set_image()
 
 window.mainloop()
